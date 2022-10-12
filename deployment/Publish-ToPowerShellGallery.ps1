d04f58bd591a92fb84f313a2ed86b7d45f4c61c7
nando <fdo.garcia.vega@gmail.com>
2022-10-12 22:30:36 +0200

    Update deployment folder

---
+++ deployment/Publish-ToPowerShellGallery.ps1
@@ -0,0 +1,9 @@
+﻿$releasePath = Join-Path $PSScriptRoot "bin\Release"
+$modulePath = Join-Path $PSScriptRoot "bin\iPowerShellSmbios"
+
+Write-Host "Preparing module"
+
+# Moverse a la carpeta de la versión elegida y ejecutar el comando.
+# Ex. versión 1.0.0.0 -> cd ...iPowerShellSmbios\deployment\v1.0.0.0
+# Cuando solicite ApiKey introducir clave
+Publish-Module -Path .\iPowerShellSmbios -Repository PSGallery -NuGetApiKey (Read-Host "NuGetApiKey (from https://powershellgallery.com/account)") -verbose
